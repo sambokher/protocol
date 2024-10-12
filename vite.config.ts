673ca38a077f6aa1e85e5efeccb3f9fdb3c674d3
sambokher <53951518+sambokher@users.noreply.github.com>
2024-10-12 14:16:10 -0400

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
-    outDir: "out/pb_public",
+    outDir: "out/pb_public", // Custom output directory
     sourcemap: false,
   },
-  envDir: '.'
-});
+  envDir: ".",
+  // Use "/" for local dev, "/protocol/" for GitHub Pages
+  base: mode === "production" ? "/protocol/" : "/",
+}));
